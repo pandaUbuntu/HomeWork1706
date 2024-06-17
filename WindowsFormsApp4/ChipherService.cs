3cc9a35c07a39dbc20402904814d6ff7a95d8c7a
Fokk <fokk190e@gmail.com>
2024-06-17 18:37:24 +0300

    Added service

---
+++ WindowsFormsApp4/ChipherService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace WindowsFormsApp4
+{
+    public class ChipherService
+    {
+        string alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+        private int loopIndex(int index, int key)
+        {
+            if (index + key >= alphabet.Length)
+            {
+                return Math.Abs(alphabet.Length - (index + key));
+            }
+            else
+            {
+                return index + key;
+            }
+        }
+
+        public string generateChipher(string text, int key)
+        {
+            string result = "";
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ')
+                {
+                    result += text[i];
+                }
+                else
+                {
+                    int index = loopIndex(alphabet.IndexOf(text[i]), key);
+
+                    result += alphabet[index];
+                }
+            }
+
+            return result;
+        }
+    }
+}
--- WindowsFormsApp4/Form1.Designer.cs
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -98,6 +98,7 @@
             this.button1.TabIndex = 6;
             this.button1.Text = "Зашифрувати";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
@@ -107,6 +108,7 @@
             this.button2.TabIndex = 7;
             this.button2.Text = "Очистити";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
